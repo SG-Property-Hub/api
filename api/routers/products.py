0306d83e00f927fa6a1983294fd56d9f27a2fe2e
vietdoo <dodoviet@icloud.com>
2024-05-31 15:58:25 +0700

    add HTTPException

--- api/routers/products.py
+++ api/routers/products.py
@@ -1,44 +1,36 @@
 from . import router, collection
-from fastapi import Query
-import json
+from fastapi import Query, HTTPException
 
 @router.get("/api/products")
 def get_products(
-                #  fields: str = Query(...),
-                 limit:     int = Query(50),
-                 category:  str = Query(None, max_length=50),
-                 dist:      str = Query(None, max_length=50),
-                 q:         str = Query(None)
-                #  offset: int = Query(0),
-                #  sort_by: str = Query(None)
-                 ):
-    
-    query= {}
-    # Query Category 
+    limit: int = Query(50),
+    category: str = Query(None, max_length=50),
+    dist: str = Query(None, max_length=50),
+    q: str = Query(None)
+):
+    query = {}
+
     if category:
         query["category"] = category
 
-    #Query dist 
     if dist:
         query["dist"] = dist
 
-    #Query string title
     if q:
-        query["title"]= { "$regex":f".*{q}.*"}
+        query["title"] = {"$regex": f".*{q}.*"}
 
-    data = collection.find(query,{'_id': 0}).limit(limit)
-    data = list(data)
-    if len(data) == 0:
-        return {"Error": "No Data"}
-    else:
-        products = data
-        return products
+    data = list(collection.find(query, {'_id': 0}).limit(limit))
+    
+    if not data:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    return data
 
 @router.get("/api/product")
-def get_product(
-                    id:        str = Query(None, max_length=50),
-                ):
-    product = collection.find_one({"raw_id":id},{'_id': 0})
+def get_product(id: str = Query(None, max_length=50)):
+    product = collection.find_one({"raw_id": id}, {'_id': 0})
+    
     if not product:
-        return {"error":"Not found"}
+        raise HTTPException(status_code=404, detail="Not found")
+        
     return product
